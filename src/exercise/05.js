fc009219601d1bc3adf3b28a9de53f144a538414
Sergey <ryabovsergey12@gmail.com>
2020-11-11 06:18:06 +0100

    exercise 5 [extra 2]

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,51 +1,20 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-const Box = props => {
+const Box = ({size = 'small', style, children}) => {
   return (
-    <div
-      className={`box ${props.className}`}
-      style={{fontStyle: 'italic', ...props.style}}
-    >
-      {props.children}
+    <div className={`box box--${size}`} style={{fontStyle: 'italic', ...style}}>
+      {children}
     </div>
   )
 }
 
-const smallBox = (
-  <div
-    className="box box--small"
-    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
-  >
-    small lightblue box
-  </div>
-)
-const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="box box--large"
-    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
-  >
-    large orange box
-  </div>
-)
-
 function App() {
   return (
     <div>
-      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
         small lightblue box
       </Box>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
     </div>
   )
 }
